a1688ff4df7af0e7b855bbfea864bd56dc05397d
deontitus <deontitus4558@gmail.com>
2026-04-21 14:20:23 +0530

    Day 11 Completion

---
+++ day12/courses/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def home_view(request):
+    return HttpResponse("Welcome to Klaw Courses")
+
+def detail_view(request, name):
+    return HttpResponse(f"Course Name: {name}")
